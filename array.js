1d46abb48f512c1fe01da1030ff620378f6dfdcd
Adam Christoper <adamchris002@gmail.com>
2022-07-24 12:41:48 +0700

    file soal latihan

--- array.js
+++ array.js
@@ -1,85 +1,85 @@
 //soal nomor 1 a
 
-// let watches = ["Swiss Army", "Daniel Wellington", "Dublot"];
+let watches = ["Swiss Army", "Daniel Wellington", "Dublot"];
 
-// var string = "Saya punya ";
+var string = "Saya punya ";
 
-// for (var i = 0; i < watches.length - 1; i++) {
-//     string += watches[i] + ", ";
-// }
-// string += "dan " + watches[watches.length - 1];
+for (var i = 0; i < watches.length - 1; i++) {
+    string += watches[i] + ", ";
+}
+string += "dan " + watches[watches.length - 1];
 
-// console.log(string + ".");
+console.log(string + ".");
 
 
 //soal nomor 1 b
 
-// var string1 = "Saya punya ";
+var string1 = "Saya punya ";
 
-// let arloji = ['A', 'B', 'C', 'D','E']
+let arloji = ['A', 'B', 'C', 'D','E']
 
-// for (var i = 0; i < arloji.length - 1; i++) {
-//     string1 += arloji[i] + ", ";
-// }
+for (var i = 0; i < arloji.length - 1; i++) {
+    string1 += arloji[i] + ", ";
+}
 
-// string1 += "dan " + arloji[arloji.length - 1];
+string1 += "dan " + arloji[arloji.length - 1];
 
-// console.log(string1 + ".");
+console.log(string1 + ".");
 
 
 //soal nomor 2
 
-// var koleksi = [];
+var koleksi = [];
 
-// var koleksia = [];
+var koleksia = [];
 
-// var kamus = "abcdefghijklmnopqrstuvwxyz";
+var kamus = "abcdefghijklmnopqrstuvwxyz";
 
-// let fruits = ["cherry","blueberry","avocado","apple","cranberry"];
+let fruits = ["cherry","blueberry","avocado","apple","cranberry"];
 
-// // console.log(fruits[0][0]);
+// console.log(fruits[0][0]);
 
-// for (var i = 0; i < kamus.length; i++) {
-//     for (var j = 0; j < fruits.length; j++) {
-//         if (kamus[i] == fruits[j][0]) {
-//             koleksia.push(fruits[j]);
+for (var i = 0; i < kamus.length; i++) {
+    for (var j = 0; j < fruits.length; j++) {
+        if (kamus[i] == fruits[j][0]) {
+            koleksia.push(fruits[j]);
             
-//         }
-//     }
-//     if (koleksia.length == 0) {
-//         continue;
-//     }
-//     else {
-//         koleksi.push(koleksia); 
-//     }
+        }
+    }
+    if (koleksia.length == 0) {
+        continue;
+    }
+    else {
+        koleksi.push(koleksia); 
+    }
     
-//     koleksia = []; 
-// }
-// console.log(koleksi);
+    koleksia = []; 
+}
+console.log(koleksi);
 
 //soal nomor 3
 
-// let kalimat = "belajar bersama dunia coding yuk";
+let kalimat = "belajar bersama dunia coding yuk";
 
-// var hasil = []
-// var sentence = ""
-// var count = 0;
+var hasil = []
+var sentence = ""
+var count = 0;
 
-// for (var i = 0; i <= kalimat.length; i++) {
-//     if (kalimat[i] !== " ") {
-//         sentence += kalimat[i];
-//     }
-//     else {
-//         hasil.push(sentence);
-//         sentence = "";
-//     }
-//     if(i === kalimat.length - 1){
-//         hasil.push(sentence);
-//         sentence = "";
-//     }
-// }
+for (var i = 0; i <= kalimat.length; i++) {
+    if (kalimat[i] !== " ") {
+        sentence += kalimat[i];
+    }
+    else {
+        hasil.push(sentence);
+        sentence = "";
+    }
+    if(i === kalimat.length - 1){
+        hasil.push(sentence);
+        sentence = "";
+    }
+}
 
 
-// console.log(hasil);
+console.log(hasil);
 
 
--- looping.js
+++ looping.js
@@ -13,38 +13,38 @@ console.log(string);
 
 //SOAL NOMOR 2
 
-// var n = 5;
-// var string = ""
-// for (var i = 1; i <= n; i++) {
-//     for (var j = 1; j <= i; j++) {
-//         if (j % 2 == 0) {
-//             string += "X"
-//         }
-//         else {
-//             string += "#"
-//         }
-//     }
-//     string += "\n"
-// }
+var n = 5;
+var string = ""
+for (var i = 1; i <= n; i++) {
+    for (var j = 1; j <= i; j++) {
+        if (j % 2 == 0) {
+            string += "X"
+        }
+        else {
+            string += "#"
+        }
+    }
+    string += "\n"
+}
 
-// console.log(string)
+console.log(string)
 
 //SOAL NOMOR 3
 
-// var nomor = 5
-// var string = ""
+var nomor = 5
+var string = ""
 
-// for (var i = 0; i < nomor; i++) {
-//     for (var j = 0; j < nomor; j++) {
-//         if (j == 0 || j == nomor - 1 || i == 0 || i == nomor -1) {
-//             string += "*"
-//         }
-//         else {
-//             string += " "
-//         }
-//     }
-//     string += "\n"
-// }
+for (var i = 0; i < nomor; i++) {
+    for (var j = 0; j < nomor; j++) {
+        if (j == 0 || j == nomor - 1 || i == 0 || i == nomor -1) {
+            string += "*"
+        }
+        else {
+            string += " "
+        }
+    }
+    string += "\n"
+}
 
-// console.log(string)
+console.log(string)
 
