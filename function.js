0d83f2a380570cb4115cc8759b58d4881cc3c302
Adam Christoper <adamchris002@gmail.com>
2022-07-24 15:25:05 +0700

    soal latihan functions

--- function.js
+++ function.js
@@ -1,1 +1,57 @@
+// soal 1
 
+// function printNumbers(num) {
+//     var temp = [];
+//     var newnum = 1;
+//     while (temp.length != num) {
+//         var baru = countPrime(newnum);
+//         if (baru === 2) {
+//             temp.push(newnum);
+//         }
+//         newnum++;
+//     }
+
+//     console.log(temp);
+// }
+
+// printNumbers(10);
+
+// function countPrime(num) {
+//     var temp = [];
+//     for (var i = 1; i <= num; i++) {
+//         if(num % i == 0) {
+//             temp.push(i);
+//         }
+//     }
+//     return temp.length;
+// }
+
+
+// printPrimes(10);
+
+//soal nomor 2
+
+// function triple(a, b, c) {
+//     a = a * a;
+//     b = b * b;
+//     c = c * c;
+
+//     var hasil;
+
+//     if ((a + b) == c) {
+//         hasil = true;
+//         console.log(hasil);
+//     }
+//     else {
+//         hasil = false;
+//         console.log(hasil)
+//     }
+// }
+
+// triple(3,4,5);
+// triple(3,3,3);
+// triple(6,8,10);
+
+//soal nomor 3
+
+let input = "5279312"
